1cf8e60c9df480ec9d2fc02f7e24e81440104dee
kuya-ui <kuyamaxmillan@gmail.com>
2021-06-23 09:32:47 +0300

    make changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-$("#col").hover(function){
-    $(#text).fadeTo("slow1");
-    $(.img-fluid).fadeTo("slow" 0.4);
-}function(){
-    $("#text").fadeTo("slow", 0);
-    $(".img-fluid").fadeTo("slow", 1);
+//$("#col").hover(function){
+    //$(#text).fadeTo("slow1");
+  //  $(.img-fluid).fadeTo("slow" 0.4);
+//}function(){
+    //$("#text").fadeTo("slow", 0);
+  //  $(".img-fluid").fadeTo("slow", 1);
+//}
+$(document).ready(function){
+    $("div#design").click(function){
+        $("div#content").toggle();
+        $("div#design").toggle();
+    }
+});
+$("div#content").click(function){
+    $("div#content").toggle();
+    $("div#design").toggle();s
 }
