550b71a070253102a6fa8053113e6dfc1caacfce
kuya-ui <kuyamaxmillan@gmail.com>
2021-06-23 07:16:36 +0300

    add portfolio

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-$(document).ready(function(){
-    $("#main").mouseover(function(){
-        $("#maxmillan").show();
-    })
-})
-$(document).ready(function(){
-    $("#main").mouseout(function(){
-        $("#maxmillan").hide();
-    })
-})
+$("#col").hover(function){
+    $(#text).fadeTo("slow1");
+    $(.img-fluid).fadeTo("slow" 0.4);
+}function(){
+    $("#text").fadeTo("slow", 0);
+    $(".img-fluid").fadeTo("slow", 1);
+}
