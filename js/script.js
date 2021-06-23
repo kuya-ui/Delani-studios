7918d076e48a1cb1666ead284562fb76264c0e2d
kuya-ui <kuyamaxmillan@gmail.com>
2021-06-23 14:20:32 +0300

    make changes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,17 @@
-//$("#col").hover(function){
-    //$(#text).fadeTo("slow1");
-  //  $(.img-fluid).fadeTo("slow" 0.4);
-//}function(){
-    //$("#text").fadeTo("slow", 0);
-  //  $(".img-fluid").fadeTo("slow", 1);
-//}
-//$(document).ready(function){
-    //$("div#design").click(function){
-    //    $("div#content").toggle();
-  //      $("div#design").toggle();
-//});
-//$("div#content").click(function){
-    //$("div#content").toggle();
-  //  $("div#design").toggle();s
-//}
+
+// $(document).ready(function(){
+//     $("#main").mouseover(function(){
+//         $("#maxmillan").show();
+//     });
+// });
+// $(document).ready(function(){
+//     $("#main").mouseout(function(){
+//         $("maxmillan").hide();
+//     });
+// });
 $(document).ready(function(){
-    $("#main").mouseover(function(){
-    $("#maxmillan").show();
+    $(".image3").click(function(){
+        $("#product").slideToggle();
+        $("#prod-contentt").slideToggle();
     });
-    });
-    $(document).ready(function(){
-    $("#main").mouseout(function(){
-    $("#maxmillan").hide();
 });
