2b99c8bcfdcf671a238a7003cc26f36af17da6ed
kuya-ui <kuyamaxmillan@gmail.com>
2021-06-23 12:28:36 +0300

    make changes

--- js/script.js
+++ js/script.js
@@ -5,13 +5,21 @@
     //$("#text").fadeTo("slow", 0);
   //  $(".img-fluid").fadeTo("slow", 1);
 //}
-$(document).ready(function){
-    $("div#design").click(function){
-        $("div#content").toggle();
-        $("div#design").toggle();
-    }
+//$(document).ready(function){
+    //$("div#design").click(function){
+    //    $("div#content").toggle();
+  //      $("div#design").toggle();
+//});
+//$("div#content").click(function){
+    //$("div#content").toggle();
+  //  $("div#design").toggle();s
+//}
+$(document).ready(function(){
+    $("#main").mouseover(function(){
+    $("#maxmillan").show();
+    });
+    });
+    $(document).ready(function(){
+    $("#main").mouseout(function(){
+    $("#maxmillan").hide();
 });
-$("div#content").click(function){
-    $("div#content").toggle();
-    $("div#design").toggle();s
-}
